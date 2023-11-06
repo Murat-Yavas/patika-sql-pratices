08614c3b4c120f48887713c7f830bb01e4788204
Murat Yavas <70375919+Murat-Yavas@users.noreply.github.com>
2023-11-06 17:59:43 +0300

    Add files via upload

---
+++ HW7/hw7.sql
@@ -0,0 +1,20 @@
+--1. Madde
+SELECT rating FROM film
+GROUP BY rating;
+
+--2. Madde
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+
+--3. Madde
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+
+--4. Madde
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
